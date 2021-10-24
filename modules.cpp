d8020a477d7e5681ff7500848e74bf2dfac9bf11
olavocinacio <olavo1611@gmail.com>
2021-10-24 15:38:29 -0300

    Configuração do NRF e do BMP280

--- modules.cpp
+++ modules.cpp
@@ -3,15 +3,129 @@
 //ESP32
 //StepDown
 
-//BNO055
-//BMP280
-//NRF24L01
-//SD Module
+//BNO055 - OK
+//BMP280 - OK
+//NRF24L01 - OK
+//SD Module - 
+//SERVO - OK
 
-#include "BNO055ESP32.h";
+
+//Bibliotecas padrão C 
+#include <stdio.h> //
+#include <stdlib.h> //
+#include <string.h> //Manipulação de strings
+
+//Biblioteca NRF24L01
+#include "mirf.h"
+
+//Biblioteca de gerenciamento de tarefas - freeRTOS
 #include "freertos/FreeRTOS.h";
 #include "freertos/task.h";
+
+//Biblioteca para logs (prints de informação no console)
+#include "esp_log.h"
+
+//Biblioteca do BNO055
+#include "BNO055ESP32.h";
+
+//Biblioteca Servo
 #include "servoControl.h"
+
+//Bibliotecas BMP280
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BMP280.h>
+//Definindo Pinos BMP
+#define BMP_SCK 13
+#define BMP_MISO 12
+#define BMP_MOSI 11
+#define BMP_CS 10
+
+//Definindo classe MYDATA_t para utilização no rádio
+typedef union {
+  uint8_t value[4];
+  unsigned long now_time;
+} MYDATA_t;
+//Criando uma variável do tipo MYDATA_t
+MYDATA_t mydata;
+
+//Configuração Receiver
+if(CONFIG_RECEIVER){
+void receiver(void *pvParameters)
+{
+	NRF24_t dev;
+
+	ESP_LOGI(pcTaskGetTaskName(0), "Start");
+	ESP_LOGI(pcTaskGetTaskName(0), "CONFIG_CE_GPIO=%d",CONFIG_CE_GPIO);
+	ESP_LOGI(pcTaskGetTaskName(0), "CONFIG_CSN_GPIO=%d",CONFIG_CSN_GPIO);
+	spi_master_init(&dev, CONFIG_CE_GPIO, CONFIG_CSN_GPIO, CONFIG_MISO_GPIO, CONFIG_MOSI_GPIO, CONFIG_SCLK_GPIO);
+
+	Nrf24_setRADDR(&dev, (uint8_t *)"FGHIJ");
+	uint8_t payload = sizeof(mydata.value);
+	uint8_t channel = 90;
+	Nrf24_config(&dev, channel, payload);
+	Nrf24_printDetails(&dev);
+	ESP_LOGI(pcTaskGetTaskName(0), "Listening...");
+
+	while(1) {
+		if (Nrf24_dataReady(&dev)) { //When the program is received, the received data is output from the serial port
+			Nrf24_getData(&dev, mydata.value);
+			ESP_LOGI(pcTaskGetTaskName(0), "Got data:%lu", mydata.now_time);
+		}
+		vTaskDelay(1);
+	}
+}
+}
+
+//Confiração transmitter
+if(CONFIG_TRANSMITTER){
+void transmitter(void *pvParameters)
+{
+	NRF24_t dev;
+
+	ESP_LOGI(pcTaskGetTaskName(0), "Start");
+	ESP_LOGI(pcTaskGetTaskName(0), "CONFIG_CE_GPIO=%d",CONFIG_CE_GPIO);
+	ESP_LOGI(pcTaskGetTaskName(0), "CONFIG_CSN_GPIO=%d",CONFIG_CSN_GPIO);
+	spi_master_init(&dev, CONFIG_CE_GPIO, CONFIG_CSN_GPIO, CONFIG_MISO_GPIO, CONFIG_MOSI_GPIO, CONFIG_SCLK_GPIO);
+
+	Nrf24_setRADDR(&dev, (uint8_t *)"ABCDE");
+	uint8_t payload = sizeof(mydata.value);
+	uint8_t channel = 90;
+	Nrf24_config(&dev, channel, payload);
+	Nrf24_printDetails(&dev);
+
+	while(1) {
+		mydata.now_time = xTaskGetTickCount();
+		Nrf24_setTADDR(&dev, (uint8_t *)"FGHIJ");			//Set the receiver address
+		Nrf24_send(&dev, mydata.value);				   //Send instructions, send random number value
+		vTaskDelay(1);
+		ESP_LOGI(pcTaskGetTaskName(0), "Wait for sending.....");
+		if (Nrf24_isSend(&dev)) {
+			ESP_LOGI(pcTaskGetTaskName(0),"Send success:%lu", mydata.now_time);
+		} else {
+			ESP_LOGI(pcTaskGetTaskName(0),"Send fail:");
+		}
+		vTaskDelay(1000/portTICK_PERIOD_MS);
+	}
+}
+}
+	
+#define tag "MIRF"
+
+void app_main(void)
+{
+    if (CONFIG_RECEIVER){
+        xTaskCreate(receiver, "RECV", 1024*2, NULL, 2, NULL);
+    }
+        // Create Task
+
+    if (CONFIG_TRANSMITTER){
+        xTaskCreate(transmitter, "TRANS", 1024*2, NULL, 2, NULL);
+
+    }
+}
+
 
 extern "C" void app_main(){
     
@@ -40,6 +154,11 @@ extern "C" void app_main(){
     // storedOffsets.magRadius = 662;
 
     //Relizar comunicação com BNO: Conferir se é GPIO ou UART
+    
+     
+    if (!bme.begin()) {
+        ESP_LOGI(TAG, "Não foi possível encontrar uma conexão válida");
+    }
 
     //inicializando o BNO e retornando erros caso não seja possível
     try{
@@ -54,6 +173,7 @@ extern "C" void app_main(){
         ESP_LOGE(TAG, "Falha na configuração, Erro %s", ex.what());
         return; //Ver possibilidade de juntar em um catch só
     }
+
 
     //Registrando dados do BNO
      try {
@@ -78,7 +198,18 @@ extern "C" void app_main(){
     // Equivalente ao void loop (uma rotina ativa durante toda a execução)
     while (1) {
 
-        if(abrir_paraquedas){
+        // Mostrando informações do BMP
+        temp = bme.readTemperature();
+        press = bme.readPressure();
+        alt = bme.readAltitude(1013.25);
+        ESP_LOGI(TAG, "TEMP: %.2f ºC", temp);
+        ESP_LOGI(TAG, "PRESS: %.2f Pa", press);
+        ESP_LOGI(TAG, "HEIGHT: %.2f m", alt);
+
+
+        estado = abrir_paraquedas();
+
+        if(estado == true){
         	myServo.write(90);
 			vTaskDelay(10 / portTICK_RATE_MS);
         }
